030bca0e19b2a0de84c17e5b7d515645f40ddf18
cybergla <tanaydeshmukh@gmail.com>
2015-07-21 22:32:44 +0530

    new Ac color, article view changed

--- app/src/main/java/vit/vithandbook/activity/ArticleActivity.java
+++ app/src/main/java/vit/vithandbook/activity/ArticleActivity.java
@@ -1,17 +1,22 @@
 package vit.vithandbook.activity;
 
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import vit.vithandbook.R;
 import vit.vithandbook.helperClass.DataBaseHelper;
@@ -21,37 +26,46 @@ import vit.vithandbook.helperClass.XmlParseHandler;
 public class ArticleActivity extends ActionBarActivity {
 
     String topic;
-    LinearLayout mainArticleLAyout;
+    String subtopicName = "";
+    String mainCategory = "";
+    LinearLayout mainArticleLayout;
     XmlParseHandler parser;
     ProgressBar load;
+    TextView title,subtopic,circletopic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        //getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
-        mainArticleLAyout = (LinearLayout) findViewById(R.id.mainArticleLayout);
+        mainArticleLayout = (LinearLayout) findViewById(R.id.mainArticleLayout);
         load = (ProgressBar) findViewById(R.id.aaProgressbar);
-        parser = new XmlParseHandler(this, mainArticleLAyout);
-        initalizeActionBar();
+        parser = new XmlParseHandler(this, mainArticleLayout);
+        topic = getIntent().getStringExtra("topic");
+        title = (TextView) findViewById(R.id.tv_title);
+        subtopic = (TextView) findViewById(R.id.tv_subtopic);
+        circletopic = (TextView) findViewById(R.id.tv_circle_topic);
+
+
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected void onPreExecute() {
                 load.setVisibility(View.VISIBLE);
             }
-
             @Override
             protected Void doInBackground(Void... params) {
-
                 fetchContent();
                 return null;
             }
-
             @Override
             protected void onPostExecute(Void res) {
                 load.setVisibility(View.GONE);
+                initalize();
             }
         }.execute();
+
+
+
     }
 
     @Override
@@ -74,13 +88,41 @@ public class ArticleActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    void initalizeActionBar() {
-        topic = getIntent().getStringExtra("topic");
-        int color = getIntent().getIntExtra("color", 0);
-        setTitle(topic);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color));
+    void initalize() {
+
+        setTitle("");
+        title.setText(topic);
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(android.R.color.background_light));
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        subtopic.setText(subtopicName);
+        circletopic.setText(mainCategory.substring(0, 2));
+        Resources colors = getResources();
+        switch (mainCategory) {
+            case "Academics":
+                ((GradientDrawable)circletopic.getBackground()).setColor(colors.getColor(R.color.academics));
+                break;
+            case "College":
+                ((GradientDrawable)circletopic.getBackground()).setColor(colors.getColor(R.color.college));
+                break;
+            case "Hostel":
+                ((GradientDrawable)circletopic.getBackground()).setColor(colors.getColor(R.color.hostel));
+                break;
+            case "Student Organisations":
+                ((GradientDrawable)circletopic.getBackground()).setColor(colors.getColor(R.color.stud));
+                break;
+            case "Life Hacks":
+                ((GradientDrawable)circletopic.getBackground()).setColor(colors.getColor(R.color.lifehack));
+                break;
+            case "Around Vit":
+                ((GradientDrawable)circletopic.getBackground()).setColor(colors.getColor(R.color.around));
+                break;
+            default:
+                circletopic.setBackgroundColor(colors.getColor(R.color.academics));
+        }
+
 
     }
 
@@ -89,9 +131,11 @@ public class ArticleActivity extends ActionBarActivity {
         Cursor cursor = null;
         try {
             db = SQLiteDatabase.openDatabase(DataBaseHelper.DB_PATH + DataBaseHelper.DB_NAME, null, SQLiteDatabase.OPEN_READWRITE);
-            cursor = db.rawQuery("SELECT `content` from `articles` Where topic = ? Limit 1", new String[]{topic});
+            cursor = db.rawQuery("SELECT `content` , `sub_category` , `main_category` from `articles` Where topic = ? Limit 1", new String[]{topic});
             cursor.moveToFirst();
             String xmlData = cursor.getString(0);
+            subtopicName = cursor.getString(1);
+            mainCategory = cursor.getString(2);
             parser.parseXml(xmlData);
         } catch (Exception e) {
             e.printStackTrace();
