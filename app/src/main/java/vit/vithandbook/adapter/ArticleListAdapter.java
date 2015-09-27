5cf50a804d57fed0a62def10164513a4e571307d
CoderHam <hemanham@gmail.com>
2015-09-28 01:31:20 +0530

    Article view slide in and out full screen, recycler view

--- app/src/main/java/vit/vithandbook/adapter/ArticleListAdapter.java
+++ app/src/main/java/vit/vithandbook/adapter/ArticleListAdapter.java
@@ -1,7 +1,9 @@
 package vit.vithandbook.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,48 +15,68 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import vit.vithandbook.R;
+import vit.vithandbook.model.Article;
 
-public class ArticleListAdapter extends ArrayAdapter<String> {
+public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.ArticleViewHolder> {
+
     Context activity;
     int[] colors;
-    public ArrayList<String> objects;
+    onItemClickListener itemClickListener;
+    ArrayList<String> objects;
 
-    public class ViewHolder {
-        public TextView content, circle;
-        public int color;
+
+    class ArticleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    TextView circle, content;
+    ArticleViewHolder(View v)
+    {
+        super(v);
+        circle = (TextView) v.findViewById(R.id.tvCircle);
+        content = (TextView) v.findViewById(R.id.tvContent);
+        v.setOnClickListener(this);
     }
 
-    public ArticleListAdapter(Context context, int resource, ArrayList<String> objects) {
-        super(context, resource, objects);
+    public void onClick(View v) {
+        String data = objects.get(getAdapterPosition());
+        if (itemClickListener != null) {
+            itemClickListener.onItemClick(data);
+        }
+    }
+}
+
+    @Override
+    public ArticleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.article_list_item, parent, false);
+        return new ArticleViewHolder(view);
+    }
+    public ArticleListAdapter(Context context, ArrayList<String> objects) {
         activity = context;
         this.objects = objects;
         colors = activity.getResources().getIntArray(R.array.colors);
     }
 
-    public void setData(ArrayList<String> newdata)
+
+    @Override
+    public void onBindViewHolder(ArticleViewHolder holder, int position) {
+        if(position<getItemCount()) {
+            int index = Math.abs(position)%11;
+            holder.content.setText(objects.get(position));
+            ((GradientDrawable) holder.circle.getBackground()).setColor(colors[index]);
+            holder.circle.setText(String.valueOf(Character.toUpperCase(objects.get(position).charAt(0))));
+        }
+    }
+
+    public void setData(ArrayList<String> data)
     {
-        clear();
-        addAll(newdata);
-        notifyDataSetChanged();
+        objects = data ;
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        ViewHolder holder;
-        if (view == null) {
-            view = LayoutInflater.from(activity).inflate(R.layout.article_list_item, parent, false);
-            holder = new ViewHolder();
-            holder.content = (TextView) view.findViewById(R.id.tvContent);
-            holder.circle = (TextView) view.findViewById(R.id.tvCircle);
-            view.setTag(holder);
-        } else
-            holder = (ViewHolder) view.getTag();
-        int index = Math.abs(objects.get(position).hashCode())%7;
-        holder.content.setText(objects.get(position));
-        holder.color = colors[index];
-        ((GradientDrawable) holder.circle.getBackground()).setColor(holder.color);
-        holder.circle.setText(String.valueOf(Character.toUpperCase(objects.get(position).charAt(0))));
-        return view;
+    public int getItemCount() {
+        return objects.size();
+    }
+
+    public void setOnItemClickListener(onItemClickListener listener) {
+        this.itemClickListener = listener;
     }
 
 }
